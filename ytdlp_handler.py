53115b9c66da924912dbb6aa63e8eab35e190443
Pierre Wendling <pierre.wendling.4@gmail.com>
2021-09-22 22:03:41 -0400

    Added type-hinting to yt_dlp handler.
    File was also formatted using autopep8 and imports were sorted.

--- ytdlp_handler.py
+++ ytdlp_handler.py
@@ -1,22 +1,26 @@
-import yt_dlp
 import mimetypes
+from typing import Any, Dict
+
+import yt_dlp
+from quantiphy import Quantity
+from yt_dlp.YoutubeDL import sanitize_filename, sanitize_path
 
 from ffmpeg_handler import *
-from quantiphy import Quantity
-from yt_dlp.YoutubeDL import sanitize_path, sanitize_filename
 
 CANCELED = False
 
 
-def video_dl(values):
+def video_dl(values: Dict) -> None:
     global CANCELED
     CANCELED = False
     trim_start = f"{values['sH']}:{values['sM']}:{values['sS']}"
     trim_end = f"{values['eH']}:{values['eM']}:{values['eS']}"
-    ydl_opts = gen_query(values['MaxHeight'], values['Browser'], values['AudioOnly'], values['path'], trim_start, trim_end)
+    ydl_opts = gen_query(values['MaxHeight'], values['Browser'],
+                         values['AudioOnly'], values['path'], trim_start, trim_end)
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         infos_ydl = ydl.extract_info(values["url"])
-    ext = infos_ydl['audio_ext'] if (values['AudioOnly'] and infos_ydl['audio_ext'] != 'none') else infos_ydl['ext']
+    ext = infos_ydl['audio_ext'] if (
+        values['AudioOnly'] and infos_ydl['audio_ext'] != 'none') else infos_ydl['ext']
     filename = f"{sanitize_filename(infos_ydl['title'][:100])} - {infos_ydl['uploader']}.{ext}"
     full_path = sanitize_path(os.path.join(values['path'], filename))
     if values['AudioOnly']:
@@ -24,7 +28,7 @@ def video_dl(values):
         os.remove(full_path)
 
 
-def gen_query(h, browser, audio_only, path, start, end):
+def gen_query(h: int, browser: str, audio_only: bool, path: str, start: str, end: str) -> Dict[str, Any]:
     options = {'noplaylist': True, 'progress_hooks': [download_progress_bar], 'trim_file_name': 250,
                'outtmpl': os.path.join(path, "%(title).100s - %(uploader)s.%(ext)s")}
     video_format = ""
@@ -46,7 +50,8 @@ def gen_query(h, browser, audio_only, path, start, end):
         }]
     if start != "00:00:00" or end != "99:59:59":
         options['external_downloader'] = 'ffmpeg'
-        options['external_downloader_args'] = {'ffmpeg_i': ['-ss', start, '-to', end]}
+        options['external_downloader_args'] = {
+            'ffmpeg_i': ['-ss', start, '-to', end]}
     elif not audio_only:
         options["merge-output-format"] = "mp4"
     if browser != "None":
@@ -62,9 +67,10 @@ def download_progress_bar(d):
         print("Done downloading {}".format(file_tuple[1]))
     if d['status'] == 'downloading':
         downloaded = Quantity(d['downloaded_bytes'], 'B')
-        total = Quantity(d['total_bytes'], 'B') if 'total_bytes' in d.keys() else Quantity(d['total_bytes_estimate'], 'B')
+        total = Quantity(d['total_bytes'], 'B') if 'total_bytes' in d.keys(
+        ) else Quantity(d['total_bytes_estimate'], 'B')
         progress = Sg.OneLineProgressMeter('Downloading', downloaded, total, f'Downloading {media_type}', orientation='h',
-                                no_titlebar=True, grab_anywhere=True)
+                                           no_titlebar=True, grab_anywhere=True)
         if CANCELED or (not progress and downloaded < total):
             CANCELED = True
             raise ValueError

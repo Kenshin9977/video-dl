2cdd6caa66792aea136fc05447296208e60d6fd7
Kenshin <kenshin9955@hotmail.fr>
2022-08-09 14:43:10 +0200

    Update TODO

--- gui.py
+++ gui.py
@@ -492,8 +492,8 @@ if __name__ == "__main__":
     Updater().update_app()
     _video_dl()
 
-# Tweak timecode switching to the next number when entering 2 digit in a row
-# in the same box
 # Allow to updates assets (ffmpeg and ffmprobe)
 # Write tests
 # Sign updates
+# Look for vcodec/acodec target using filters with ydl
+# Fix playlist error

b6767de0fbd1621b6c58e18ebdb2675a8086361f
Kenshin <kenshin@Kenshins-MacBook-Pro.local>
2022-08-15 00:54:59 +0200

    Change the way to get the fastest encoder

--- hwaccel_handler.py
+++ hwaccel_handler.py
@@ -1,34 +1,77 @@
-from re import match
+import os
 
-from GPUtil import getGPUs
+import ffmpeg
+
+ENCODERS = {
+    "x264": {
+        "QuickSync": "h264_qsv",
+        "NVENC": "h264_nvenc",
+        "AMF": "h264_amf",
+        "Apple": "h264_videotoolbox",
+        "Raspberry": "h264_v4l2m2m",
+        "CPU": "libx264",
+    },
+    "x265": {
+        "QuickSync": "hevc_qsv",
+        "NVENC": "hevc_nvenc",
+        "AMF": "hevc_amf",
+        "Apple": "hevc_videotoolbox",
+        "Raspberry": "hevc_v4l2m2m",
+        "CPU": "libx265",
+    },
+    "ProRes": {
+        "QuickSync": None,
+        "NVENC": None,
+        "AMF": None,
+        "Apple": "prores_videotoolbox",
+        "Raspberry": None,
+        "CPU": "prores_ks",
+    },
+    "AV1": {
+        "QuickSync": None,
+        "NVENC": None,
+        "AMF": None,
+        "Apple": None,
+        "CPU": "liboam-av1",
+    },
+}
 
 
-def _get_encoders_list() -> dict:
-    encoders_dict = dict()
-    encoders_dict.update(
-        {
-            "x264": list(),
-            "x265": list(),
-            "ProRes": ["prores_ks"],
-            "AV1": ["libaom-av1"],
-        }
-    )
-    tmp_list_x264 = list()
-    tmp_list_x265 = list()
-    gpus = getGPUs()
-    for gpu in gpus:
-        gpu_name = gpu.name
-        if match("NVIDIA", gpu_name):
-            tmp_list_x264.append("h264_nvenc")
-            tmp_list_x265.append("hevc_nvenc")
-        if match("AMD", gpu_name):
-            tmp_list_x264.append("h264_amf")
-            tmp_list_x265.append("hevc_amf")
-        if match("Intel", gpu_name):
-            tmp_list_x264.append("h264_qsv")
-            tmp_list_x265.append("hevc_qsv")
-    tmp_list_x264.append("libx264")
-    tmp_list_x265.append("libx265")
-    encoders_dict["x264"] = tmp_list_x264
-    encoders_dict["x265"] = tmp_list_x265
-    return encoders_dict
+def fastest_encoder(path: str, target_vcodec: str) -> str:
+    """
+    Determine the fastest encoder by trying to encode 1 frame with each
+    possible encoder for the targeted video codec. As soon as one works it is
+    considered the fastest as it's pretty rare to have multiple hardware
+    encoders and the difference in speed/quality is often negligible.
+    The CPU is the fallback option as it's a universal solution altough the
+    slowest.
+
+    Args:
+        path (str): path to the file used to test the codecs
+        target_vcodec (str): Video codec to encode to
+
+    Raises:
+        ffmpeg.Error: If no compatible codec was found
+
+    Returns:
+        str: Fastest compatible codec
+    """
+    file_name_ext = os.path.splitext(path)
+    new_ext = ".mp4" if target_vcodec != "ProRes" else ".mov"
+    output_path = f"{file_name_ext[0]}.tmp{new_ext}"
+    vcodecs = ENCODERS[target_vcodec].values()
+    for vcodec in vcodecs:
+        if vcodec is None:
+            continue
+        try:
+            ffmpeg.input(path).output(
+                output_path, vframes=1, vcodec=vcodec
+            ).run(overwrite_output=True)
+        except ffmpeg.Error:
+            continue
+        else:
+            return vcodec
+        finally:
+            if os.path.isfile(output_path):
+                os.remove(path=output_path)
+    raise ffmpeg.Error

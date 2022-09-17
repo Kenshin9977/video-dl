32849b0e99d396df9b7ba3c9b00da1c438db4983
Kenshin <kenshin@Kenshins-MacBook-Pro.local>
2022-09-17 22:57:08 +0200

    Add WIP modals logic improvements

--- utils/gui_utils.py
+++ utils/gui_utils.py
@@ -21,10 +21,34 @@ def create_progress_bar(action: str, update: bool) -> Sg.Window:
         [Sg.Text("", key="PROGINFOS2")],
         [Sg.Cancel(button_text=get_text(GuiField.cancel_button))],
     ]
-    return Sg.Window(
+    progress_bar = Sg.Window(
         action,
         layout,
         no_titlebar=not update,
         grab_anywhere=not update,
         modal=not update,
+        keep_on_top=True
     )
+    # progress_bar.TKroot.bind('<Configure>', configure)
+    # progress_bar.TKroot.bind('<FocusIn>', focus_in)
+    return progress_bar
+
+
+# def configure(event):
+#     global user_setting
+#     global x1, x2, y1, y2
+#     if not user_setting:
+#         return
+#     x, y = win2.current_location()
+#     dx, dy = x - x2, y - y2
+#     x1, y1 = x1 + dx, y1 + dy
+#     win1.move(x1, y1)
+#     x2, y2 = x, y
+
+# def focus_in(event):
+#     global user_setting
+#     win1.bring_to_front()
+#     if win2:
+#         user_setting = True
+#         win2.bring_to_front()
+#         user_setting = False

47a9ffa030d3c5b5675edd0cfbbd395b4bd4a1d9
Kenshin9977 <kenshin9955@hotmail.fr>
2026-02-27 23:15:16 +0100

    Fix macOS dock icon: patch Flet bundle in dedicated path

--- utils/sys_utils.py
+++ utils/sys_utils.py
@@ -26,7 +26,7 @@ from utils.quickjs_install import (
 )
 
 APP_NAME = "video-dl"
-APP_VERSION = "2.1.5"
+APP_VERSION = "2.1.6"
 PLATFORM = system()
 
 _PLATFORM_SUFFIX_MAP = {"Windows": "windows", "Linux": "linux", "Darwin": "macos"}
